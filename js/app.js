ca075cb3249d6dd6872d56bdd410fd88bf51ed77
Bycor Sánchez <bycor1989@gmail.com>
2018-03-21 21:46:26 +0000

    refactor: rename deck to board

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 //Prevent variable modification from console
 {
 	const values = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-camera", "fa-camera"];
-	const deck = document.querySelector(".deck");
-	const cards = document.querySelectorAll(".deck .card");
+	const board = document.querySelector(".board");
+	const cards = document.querySelectorAll(".board .card");
 	const score = document.querySelector(".moves");
 	const restart = document.querySelector(".restart");
 
 	let matches, moves, selected;
 
-	function shuffleDeck() {
+	function shuffleCards() {
+		//Shuffle values
 	    var index = values.length, temporaryValue, randomIndex;
 	    while (index !== 0) {
 	        randomIndex = Math.floor(Math.random() * index);
@@ -36,7 +37,7 @@
 		selected = undefined;
 
 		updateScore();
-		shuffleDeck();
+		shuffleCards();
 	}
 
 	function setValue(card, value){
@@ -57,12 +58,12 @@
 		card.classList.add("wrong");
 	}
 
-	function flipDown(card){
-		card.classList.remove("show", "match", "wrong");
-	}
-
 	function flipUp(card){
 		card.classList.add("show");
+	}
+
+	function flipDown(card){
+		card.classList.remove("show", "match", "wrong");
 	}
 
 	function isNotFlippedCard(element){
@@ -70,15 +71,25 @@
 		return classList.contains("card") && !classList.contains("show");
 	}
 
-	//Init game of restart
+	function checkEnd(){
+		if (matches === 0){
+			console.log("Game finished!");
+		}
+	}
+
+/*
+ * Event listeners
+ */
+
+	//Init game on restart
 	restart.addEventListener("click", init);
 
 
 	//Select card
-	deck.addEventListener("click", event => {
+	board.addEventListener("click", event => {
 	    const card = event.target;
 
-	    //Is it a card & is it unlocked?
+	    //Is it a card & is not locked?
 	    if (isNotFlippedCard(card)){
 
 	        flipUp(card);
@@ -110,12 +121,10 @@
 	    }
 	});
 
-	function checkEnd(){
-		if (matches === 0){
-			console.log("Game finished!");
-		}
-	}
 
-	//Shuffle deck on start
+	/*
+	 * Init game
+	 */
+
 	init();
 }
