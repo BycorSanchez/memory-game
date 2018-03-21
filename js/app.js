34073537a167f035f9dadd2090b3acd5db3dad9b
Bycor Sánchez <bycor1989@gmail.com>
2018-03-21 14:29:53 +0000

    refactor: refactor production code
    refactor code using simpler functions

--- js/app.js
+++ js/app.js
@@ -1,46 +1,115 @@
-const values = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-diamond", "fa-diamond"];
+//Prevent variable modification from console
+{
+	const values = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-crosshairs", "fa-crosshairs"];
+	const deck = document.querySelector(".deck");
+	const cards = document.querySelectorAll(".deck .card");
+	const score = document.querySelector(".moves");
+	const restart = document.querySelector(".restart");
 
-const cardValues = document.querySelectorAll(".card .value");
-const restartButton = document.querySelector(".restart");
+	let matches, moves, selected;
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-const shuffle = array => {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	function shuffleDeck() {
+	    var index = values.length, temporaryValue, randomIndex;
+	    while (index !== 0) {
+	        randomIndex = Math.floor(Math.random() * index);
+	        index--;
+	        temporaryValue = values[index];
+	        values[index] = values[randomIndex];
+	        values[randomIndex] = temporaryValue;
+	    }
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+		//Flip down & set card values
+		cards.forEach((card, i) => {
+			flipDown(card);
+			setValue(card, values[i]);
+		});
+	}
 
-    return array;
-}
+	function updateScore(){
+		score.textContent = moves;
+	}
 
-const shuffleDeck = () => {
-	const shuffledValues = shuffle(values);
-	cardValues.forEach((card, i) =>{
-		card.className = "";
-		card.classList.add("fa", shuffledValues[i]);
+	function init(){
+		//Restart values
+		matches = 8;
+		moves = 0;
+		selected = undefined;
+
+		updateScore();
+		shuffleDeck();
+	}
+
+	function setValue(card, value){
+		const content = card.firstElementChild;
+		content.className = "";
+		content.classList.add("fa", value);
+	}
+
+	function value(card){
+		return card.firstElementChild.className;
+	}
+
+	function match(card){
+		card.classList.add("match");
+	}
+
+	function flipDown(card){
+		card.classList.remove("open", "show", "lock", "match");
+	}
+
+	function flipUp(card){
+		card.classList.add("open", "show", "lock");
+	}
+
+	function isUnlockedCard(element){
+		const classList = element.classList;
+		return classList.contains("card") && !classList.contains("lock");
+	}
+
+	//Init game of restart
+	restart.addEventListener("click", init);
+
+
+	//Select card
+	deck.addEventListener("click", event => {
+	    const card = event.target;
+
+	    //Is it a card & is it unlocked?
+	    if (isUnlockedCard(card)){
+
+	        flipUp(card);
+
+	        //First pick
+	        if (selected === undefined){
+	            selected = card;
+	        }
+	        //Second pick
+	        else{
+	            //Equal
+	            if (value(card) === value(selected)){
+	                matches--;
+	                match(card);
+	                match(selected);
+	            }
+	            //Not equal
+	            else{
+	                flipDown(card);
+	                flipDown(selected);
+	            }
+	            selected = undefined;
+	            moves++;
+	            updateScore();
+	        }
+	        checkEnd();
+	    }
 	});
+
+	function checkEnd(){
+		if (matches === 0){
+			console.log("Game finished!");
+		}
+	}
+
+	//Shuffle deck on start
+	init();
 }
-
-
-restartButton.addEventListener('click', shuffleDeck);
-
-
-//Shuffle deck on start
-shuffleDeck();
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
