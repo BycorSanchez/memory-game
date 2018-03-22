5bfadaed6ddaf24a6a576736afad750604b6abfb
Bycor Sánchez <bycor1989@gmail.com>
2018-03-22 09:45:26 +0000

    feat: add star behavior
    remove stars gradually when moves are larger than specific values

--- js/app.js
+++ js/app.js
@@ -1,94 +1,26 @@
 //Prevent variable modification from console
 {
 	const values = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-bicycle", "fa-bicycle", "fa-camera", "fa-camera"];
-	const board = document.querySelector(".board");
-	const score = document.querySelector(".moves");
+	const score = document.querySelector(".score-panel");
+	const starElements = score.querySelectorAll(".score-panel .fa");
 	const restart = document.querySelector(".restart");
-	const finished = document.querySelector("#finish-dialog")
+	const board = document.querySelector(".board");
+	const finished = document.querySelector(".finish-dialog");
 
-	let matches, moves, selected;
+	let matches, moves, selected, time, stars;
 
-	function shuffleValues() {
-	    var index = values.length, temporaryValue, randomIndex;
-	    while (index !== 0) {
-	        randomIndex = Math.floor(Math.random() * index);
-	        index--;
-	        temporaryValue = values[index];
-	        values[index] = values[randomIndex];
-	        values[randomIndex] = temporaryValue;
-	    }
-	}
+	/*
+	 * Init game
+	 */
 
-	function dealCards(){
-		const cards = document.querySelectorAll(".board .card");
-		cards.forEach((card, i) => {
-			flipDown(card);
-			//Prevent show value before flip down
-			setTimeout(setValue, 500, card, values[i]); 
-		});
-	}
+	init();
 
-	function updateScore(){
-		score.textContent = moves;
-	}
+	/*
+	 * Event listeners
+	 */
 
-	function init(){
-		//Restart values
-		matches = 8;
-		moves = 0;
-		selected = undefined;
-
-		finished.classList.add("hidden");
-		updateScore();
-		shuffleValues();
-		dealCards();
-	}
-
-	function setValue(card, value){
-		const content = card.firstElementChild;
-		content.className = "";
-		content.classList.add("fa", value);
-	}
-
-	function value(card){
-		return card.firstElementChild.className;
-	}
-
-	function match(card){
-		card.classList.add("match");
-	}
-
-	function wrong(card){
-		card.classList.add("wrong");
-	}
-
-	function flipUp(card){
-		card.classList.add("show");
-	}
-
-	function flipDown(card){
-		card.classList.remove("show", "match", "wrong");
-	}
-
-	function isNotFlippedCard(element){
-		const classList = element.classList;
-		return classList.contains("card") && !classList.contains("show");
-	}
-
-	function checkEnd(){
-		if (matches === 0){
-			finished.classList.remove("hidden");
-			console.log("Game finished!");
-		}
-	}
-
-/*
- * Event listeners
- */
-
-	//Init game on restart
+	//Restart game
 	restart.addEventListener("click", init);
-
 
 	//Select card
 	board.addEventListener("click", event => {
@@ -126,10 +58,97 @@
 	    }
 	});
 
+	function init(){
+		//Restart values
+		matches = 8;
+		moves = 0;
+		time = 0;
+		stars = 3;
+		selected = undefined;
 
-	/*
-	 * Init game
-	 */
+		finished.classList.add("hidden");
+		updateScore();
+		shuffle();
+		dealCards();
+	}
 
-	init();
+	function updateScore(){
+		score.querySelector(".moves").textContent = moves;
+
+		switch(moves){
+			case 0:
+				starElements.forEach((element) => element.classList.add("fa-star"));
+				break;
+			case 16:
+				stars = 2;
+				starElements[stars].classList.remove("fa-star");
+				break;
+			case 34:
+				stars = 1;
+				starElements[stars].classList.remove("fa-star");
+				break;
+			case 50:
+				stars = 0;
+				starElements[stars].classList.remove("fa-star");
+				break;
+		}
+	}
+
+	function shuffle() {
+	    var index = values.length, temporaryValue, randomIndex;
+	    while (index !== 0) {
+	        randomIndex = Math.floor(Math.random() * index);
+	        index--;
+	        temporaryValue = values[index];
+	        values[index] = values[randomIndex];
+	        values[randomIndex] = temporaryValue;
+	    }
+	}
+
+	function dealCards(){
+		const cards = board.querySelectorAll(".card");
+		cards.forEach((card, i) => {
+			flipDown(card);
+			//Prevent show value before flip down (transition 0.5s)
+			setTimeout(setValue, 500, card, values[i]); 
+		});
+	}
+
+	function checkEnd(){
+		if (matches === 0){
+			finished.classList.remove("hidden");
+			console.log("Game finished!");
+		}
+	}
+
+	function match(card){
+		card.classList.add("match");
+	}
+
+	function wrong(card){
+		card.classList.add("wrong");
+	}
+
+	function flipUp(card){
+		card.classList.add("show");
+	}
+
+	function flipDown(card){
+		card.classList.remove("show", "match", "wrong");
+	}
+
+	function value(card){
+		return card.firstElementChild.className;
+	}
+
+	function setValue(card, value){
+		const content = card.firstElementChild;
+		content.className = "";
+		content.classList.add("fa", value);
+	}
+
+	function isNotFlippedCard(element){
+		const classList = element.classList;
+		return classList.contains("card") && !classList.contains("show");
+	}
 }
